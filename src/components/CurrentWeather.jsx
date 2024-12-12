53f4576648cd868f65af8b773cc5d7bddb04cdfe
FunChosa <gilazieva.00@mail.ru>
2024-12-12 14:50:17 +0300

    icons fix

--- src/components/CurrentWeather.jsx
+++ src/components/CurrentWeather.jsx
@@ -1,10 +1,32 @@
 import React from "react";
+import clear from "../icons/clear.svg";
+import clouds from "../icons/clouds.svg";
+import mist from "../icons/mist.svg";
+import moderate_heavy_rain from "../icons/moderate_heavy_rain.svg";
+import rain from "../icons/rain.svg";
+import snow from "../icons/snow.svg";
+import thunder_rain from "../icons/thunder_rain.svg";
+import thunder from "../icons/thunder.svg";
 
 const CurrentWeather = ({ currentWeather }) => {
-  const image = `src/icons/${currentWeather.weatherIcon}.svg`;
+  const weatherIcons = {
+    clear,
+    clouds,
+    mist,
+    moderate_heavy_rain,
+    rain,
+    snow,
+    thunder,
+    thunder_rain,
+  };
+
   return (
     <div className="current-weather">
-      <img src={image} alt="icon weather" className="weather-icon" />
+      <img
+        src={weatherIcons[currentWeather.weatherIcon]}
+        alt="icon weather"
+        className="weather-icon"
+      />
       <h2 className="temperature">
         {currentWeather.temperature} <span>°C</span>
       </h2>
